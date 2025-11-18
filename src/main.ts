ca7f6a2bdaa2f9ce262ed6d1d073d60925e953c7
chanachaipmg <chanachai.pmg.eng@gmail.com>
2025-11-18 16:31:50 +0700

    s

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { registerLicense } from '@syncfusion/ej2-base';
 import { AppModule } from './app/app.module';
+
+// Register Syncfusion license (v.29)
+registerLicense('ORg4AjUWIQA/Gnt2XFhhQlJHfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTH5WdUVjWXtXdHNdRWFbWkdx');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
